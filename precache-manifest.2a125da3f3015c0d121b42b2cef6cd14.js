79665d5ec1ed08d46923dbbccd270c148376d46d
Nivedita-del <niveditamadhava1@gmail.com>
2020-05-31 22:02:23 +0530

    Updates

---
+++ precache-manifest.2a125da3f3015c0d121b42b2cef6cd14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5852dbed4a4a9f1c14ee03d6e4b9be",
+    "url": "/minhance/index.html"
+  },
+  {
+    "revision": "7e32dcee91262b2ddf51",
+    "url": "/minhance/static/js/2.d9774d21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minhance/static/js/2.d9774d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0f9cf5ef45cff5bbe2",
+    "url": "/minhance/static/js/main.0fd367e7.chunk.js"
+  },
+  {
+    "revision": "869feedc2252ee87b015",
+    "url": "/minhance/static/js/runtime-main.87753bdf.js"
+  }
+]);
